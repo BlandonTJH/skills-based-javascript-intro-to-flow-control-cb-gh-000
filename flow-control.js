1765e41ebc86a482cfd176f7681c3b318c864bc8
Blandon Tan <e170095@e.ntu.edu.sg>
2020-05-17 05:35:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,7 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch(age):
+  switch(age){
     case 13:
       return "You are a teenager"
     case 14:
@@ -47,4 +47,5 @@ function switchAge(age) {
       return "You are a teenager"
     default:
       return "You have an age"
+    }
 }
