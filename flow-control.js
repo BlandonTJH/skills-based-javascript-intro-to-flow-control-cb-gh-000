d57cfeb5b3fcf7a442b967d83deee47d83bf94a7
Blandon Tan <e170095@e.ntu.edu.sg>
2020-05-17 06:17:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,7 @@
 function basicTeenager(age) {
   if (13 <= age && age <= 19){
     return "You are a teenager!"
-  }else if (age < 13 || age > 19){
+  }else if (age < 13 && age > 19){
     return undefined
   }
 }
@@ -9,7 +9,7 @@ function basicTeenager(age) {
 function teenager(age) {
   if (13 <= age && age <= 19){
     return "You are a teenager!"
-  }else if (age < 13 || age > 19) {
+  }else if (age < 13 && age > 19) {
     return "You are not a teenager"
   }
 }
